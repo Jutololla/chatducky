c936b76273a571efa5d0b5dd607f563dd6f55321
Jutololla <julianangulop@gmail.com>
2021-10-25 11:10:45 -0500

    commit

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,14 +3,14 @@ import "firebase/compat/auth";
 import "firebase/compat/database";
 
 const config = {
-    apiKey: "AIzaSyAR0xx05ouiD9zRgfjkLpJABdABh_7-1Sw",
-    authDomain: "chaty-4b4aa.firebaseapp.com",
-    databaseURL: "https://chaty-4b4aa-default-rtdb.firebaseio.com/",
-    projectId: "chaty-4b4aa",
-    storageBucket: "chaty-4b4aa.appspot.com",
-    messagingSenderId: "957267397111",
-    appId: "1:957267397111:web:30034a3a6e979c8ee646f2",
-    //measurementId: "G-E6XDJ7B6YC"
+  apiKey: "AIzaSyCBvuTsuRau9LzGEZmTcvA_MVaQkNTSEUU",
+  authDomain: "chatduck-bfdb7.firebaseapp.com",
+  projectId: "chatduck-bfdb7",
+  storageBucket: "chatduck-bfdb7.appspot.com",
+  messagingSenderId: "1091646058827",
+  appId: "1:1091646058827:web:8f77ad772a6eced2bcb8ce",
+  databaseURL:  "https://chatduck-bfdb7-default-rtdb.firebaseio.com/"
+   
   };
   firebase.initializeApp(config);
   
